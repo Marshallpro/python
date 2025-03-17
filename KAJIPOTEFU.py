43b5889631ef0b6c4027ffeec34944e5b501821e
IRADUKUNDAEMMY <iradukundaemmy55@gmail.com>
2025-03-17 18:22:20 +0200

    this our KAJIPOTEU program

---
+++ KAJIPOTEFU.py
@@ -0,0 +1,11 @@
+print("enter any string to encript")
+name=input()
+
+for x in range(len(name)):
+     name.replace("k"or"K","A")
+     name.replace("j"or"J","I")
+     name.replace("P"or"p","O")
+     name.replace("t"or"T","E")
+     name.replace("f"or"F","U")
+     name.append()
+print(name)   
