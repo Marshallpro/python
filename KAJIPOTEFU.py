ff7c806258fd7425dfd15f3356be1119f6904019
Marshallpro <iradukundaemmy55@gmail.com>
2025-03-17 18:53:31 +0200

    Update KAJIPOTEFU.py

--- KAJIPOTEFU.py
+++ KAJIPOTEFU.py
@@ -2,10 +2,10 @@ print("enter any string to encript")
 name=input()
 
 for x in range(len(name)):
-     name.replace("k"or"K","A")
-     name.replace("j"or"J","I")
-     name.replace("P"or"p","O")
-     name.replace("t"or"T","E")
-     name.replace("f"or"F","U")
-     name.append()
+     name=name.replace("k"or"K","A")
+     name=name.replace("j"or"J","I")
+     name=name.replace("P"or"p","O")
+     name=name.replace("t"or"T","E")
+     name=name.replace("f"or"F","U")
+     
 print(name)   
